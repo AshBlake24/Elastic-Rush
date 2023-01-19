1e537abf5ccb1adcbaac46c90ed267ea423a5a17
Roman Ridel <ridel_rv@mail.ru>
2023-01-19 16:59:59 +0300

    Add ElasticBand

---
+++ Assets/Source/Collectables/ElasticBand.cs
@@ -0,0 +1,16 @@
+using ElasticRush.Core;
+using UnityEngine;
+
+namespace ElasticRush.Collectables
+{
+
+    public class ElasticBand : CollectableItem
+    {
+        protected override void OnCollected(Player player)
+        {
+            player.LevelUp();
+
+            Destroy(gameObject);
+        }
+    }
+}
