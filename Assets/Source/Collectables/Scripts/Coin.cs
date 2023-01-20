bb809ba93a660d8c52eba3a9d82e135347000c06
Roman Ridel <ridel_rv@mail.ru>
2023-01-20 10:11:59 +0300

    Reorganize folders structure

---
+++ Assets/Source/Collectables/Scripts/Coin.cs
@@ -0,0 +1,15 @@
+using ElasticRush.Core;
+using UnityEngine;
+
+namespace ElasticRush.Collectables
+{
+    public class Coin : CollectableItem
+    {
+        [SerializeField] private int _scoreAmount;
+
+        protected override void OnCollected(Player player)
+        {
+            player.AddScore(_scoreAmount);
+        }
+    }
+}
