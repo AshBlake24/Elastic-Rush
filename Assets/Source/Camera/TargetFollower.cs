0a3bccc5e75ef3c25b5092cba6e9d83565b6e50e
Roman Ridel <ridel_rv@mail.ru>
2023-01-23 15:36:28 +0300

    Fix TargetFollower

--- Assets/Source/Camera/TargetFollower.cs
+++ Assets/Source/Camera/TargetFollower.cs
@@ -6,7 +6,7 @@ namespace ElasticRush.Core
     {
         [SerializeField] private Transform _target;
 
-        private Vector3 _lastTargetRotation;
+        private Quaternion _lastTargetRotation;
         private Vector3 _positionOffset;
 
         private void Start()
@@ -32,9 +32,10 @@ namespace ElasticRush.Core
 
         private void RotateAroundTarget()
         {
-            float delta = _target.rotation.eulerAngles.y - _lastTargetRotation.y;
-            _lastTargetRotation = _target.rotation.eulerAngles;
-
+            float delta = _target.rotation.y - _lastTargetRotation.y;
+            _lastTargetRotation = _target.rotation;
+            Debug.Log("Last " + _lastTargetRotation);
+            Debug.Log("Delta " + delta);
             transform.RotateAround(_target.position, Vector3.up, delta);
 
             UpdatePositionOffset();
