7602a758eb9d1b952f9f849bac3268c362b6d14b
Roman Ridel <ridel_rv@mail.ru>
2023-02-13 16:50:54 +0300

    Start working on leaderboard

---
+++ Assets/Source/Core/Leaderboard/LeaderboardEntry.cs
@@ -0,0 +1,21 @@
+using Agava.YandexGames;
+
+namespace ElasticRush
+{
+    public class LeaderboardEntry
+    {
+        public string Rank { get; private set; }
+        public string PlayerName { get; private set; }
+        public string Score { get; private set; }
+
+        public LeaderboardEntry(LeaderboardEntryResponse entry)
+        {
+            if (entry == null)
+                return;
+
+            Rank = entry.rank.ToString();
+            PlayerName = entry.player.publicName;
+            Score = entry.score.ToString();
+        }
+    }
+}
