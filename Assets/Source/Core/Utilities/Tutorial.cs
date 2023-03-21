8cf9ab302d0de9c878081742b969e132944f6435
Roman Ridel <ridel_rv@mail.ru>
2023-03-21 14:17:31 +0300

    Update Tutorial

--- Assets/Source/Core/Utilities/Tutorial.cs
+++ Assets/Source/Core/Utilities/Tutorial.cs
@@ -2,24 +2,36 @@ using System.Collections;
 using UnityEngine;
 using ElasticRush.Utilities;
 using TMPro;
+using Agava.YandexGames;
 
 namespace ElasticRush.Core
 {
     public class Tutorial : MonoBehaviour
     {
         [SerializeField] private TMP_Text _text;
-        [SerializeField] private Animator _tapAnimation;
+        [SerializeField] private Animator _touchAnimation;
+        [SerializeField] private Animator _keyboardhAnimation;
         [SerializeField] private float _animationDuration;
+
+        private Animator _currentAnmimation;
 
         private void Start()
         {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            if (Device.Type == Agava.YandexGames.DeviceType.Desktop)
+                _currentAnmimation = _keyboardhAnimation;
+            else
+                _currentAnmimation = _touchAnimation;
+#else
+            _currentAnmimation = _keyboardhAnimation;
+#endif
             TurnOn();
         }
 
         public void TurnOn()
         {
             _text.gameObject.SetActive(true);
-            _tapAnimation.gameObject.SetActive(true);
+            _currentAnmimation.gameObject.SetActive(true);
         }
 
         public void TurnOff()
@@ -32,7 +44,7 @@ namespace ElasticRush.Core
         {
             yield return Helpers.GetTime(_animationDuration);
 
-            _tapAnimation.gameObject.SetActive(false);
+            _currentAnmimation.gameObject.SetActive(false);
         }
     }
 }
